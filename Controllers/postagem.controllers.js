62516ecb56c8ebc5d7d45410fe3653af06fa2c9d
Pedro Nogueira <pedro.nogueira.barboza@gmail.com>
2024-03-23 09:31:13 -0300

    terminei as rotas de postagem.

--- Controllers/postagem.controllers.js
+++ Controllers/postagem.controllers.js
@@ -4,10 +4,35 @@ const prisma = new PrismaClient();
 
 
 export const getPostagens = async (req, res) => {
+    const postagens = await prisma.postagem.findMany({
+        include: {
+            categoria: true
+        }
+    })
 
+    res.json({
+        data: postagens,
+        message: 'Postagens encontradas com sucesso'
+    })
 }
 
-export const getPostagemPorId = async (req, res) => {}
+export const getPostagemPorId = async (req, res) => {
+
+    const postagem = await prisma.postagem.findUnique({
+        where: {
+            id: parseInt(req.params.postagemId)
+        },
+
+        include: {
+            categoria: true
+        }
+    })
+
+    res.json({
+        data: postagem,
+        message: 'Postagens encontradas com sucesso'
+    })
+}
 
 export const criarPostagem = async (req, res) => {
     const postagem = await prisma.postagem.create({
@@ -32,7 +57,38 @@ export const criarPostagem = async (req, res) => {
 
 }
 
-export const atualizarPostagem = async (req, res) => {}
+export const atualizarPostagem = async (req, res) => {
+    const postagem = await prisma.postagem.update({
+        where: {
+            id: parseInt(req.params.postagemId)
+        },
+        data: {
+            titulo: req.body.titulo,
+            conteudo: req.body.conteudo,
+            categoria: {
+                connect: req.body.categoriasIds // É um array de ids
+            }
+        }
+    })
 
-export const deletarPostagem = async (req, res) => {}
+    res.json({
+        data: postagem,
+        message: 'Postagem atualizada com sucesso'
+    })
+
+}
+
+export const deletarPostagem = async (req, res) => {
+
+    const postagemDeletada = await prisma.postagem.delete({
+        where: {
+            id: parseInt(req.params.postagemId)
+        }
+    })
+
+    res.json({
+        data: postagemDeletada,
+        message: 'Postagem deletada com sucesso'
+    })
+}
 
